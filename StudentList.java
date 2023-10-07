aa7e589e1773bb9c7def0b074db603baa8c9836a
mdomorffaruk <mdomorffaruk@gmail.com>
2023-10-07 22:29:42 +0600

    run0, run 1 complete

--- StudentList.java
+++ StudentList.java
@@ -25,11 +25,13 @@ public class StudentList {
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
 			String r = s.readLine();
-			System.out.println(r);
-			String i[] = r.split(",");	
+			// System.out.println(r);
+			String i[] = r.split(",");
+			System.out.println(i[2]);	
 			Random x = new Random();
 				int y = x.nextInt();
 					System.out.println(i[y]);
+
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");			
 		}
